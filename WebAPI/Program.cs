1732d06e351569d391747a45993837da23bf6255
Mehmet Çomak <mehmet.comak@brands-a.com>
2024-01-29 10:59:07 +0300

    refactoring

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -30,7 +30,6 @@ namespace WebAPI
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
-
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
